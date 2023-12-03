a1e98bc73d5c46470f89baec254bb4e3cc033314
DSam327 <113661235+DSam327@users.noreply.github.com>
2023-12-03 17:09:55 +0530

    implementing the frontend part with react

---
+++ notesapp/src/pages/NotePage.js
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react'
+import { useParams, Link } from 'react-router-dom';
+import { ReactComponent as ArrowLeft } from '../assets/arrow-left.svg';
+
+const NotePage = (props) => {
+    let n1=useParams()
+    console.log(n1)
+    let noteid=n1.id
+    let [note, setNote]=useState(null)
+    
+    useEffect(()=>{
+        getnote()
+    }, [noteid])
+
+    let updateNote = async () => {
+        await fetch(`http://127.0.0.1:8000/api/notes/${noteid}`, {
+            method : 'PUT', 
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body : JSON.stringify({...note, 'updated': new Date()})
+        })
+    }
+
+    let createNote = async () => {
+        await fetch(`http://127.0.0.1:8000/api/notes/`, {
+            method : 'POST', 
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body : JSON.stringify({...note, 'updated': new Date()})
+        })
+    }
+
+    let deleteNote = async () => {
+        await fetch(`http://127.0.0.1:8000/api/notes/${noteid}`, {
+            method : 'DELETE', 
+            headers: {
+                'Content-type' : 'application/json'
+            },
+            body : JSON.stringify(note)
+        })
+    }
+
+    let getnote = async () => {
+        if(noteid==='new')return
+        let response = await fetch(`http://127.0.0.1:8000/api/notes/${noteid}`)
+        let data = await response.json()
+        setNote(data)
+    }
+
+    let handleSubmit = () => {
+        if(noteid!=='new' && !note.body){
+            deleteNote()
+        }
+        else if(noteid!=='new'){
+            updateNote()
+        }
+        else if(note!==null){
+            createNote()
+        }
+    }
+
+  return (
+    <div className='note'>
+        <div className='note-header'>
+            <h3>
+                <Link to={"/"}>
+                    <ArrowLeft onClick={handleSubmit} />
+                </Link>
+            </h3>
+            <Link to={"/"}>
+            {noteid!=='new'? (<button onClick={deleteNote}>Delete</button>): (<button onClick={createNote}>Done</button>)}
+                    
+            </Link>
+        </div>
+        <textarea onChange={(e)=>{setNote({...note, 'body':e.target.value})}} value={note?.body}>
+        </textarea>
+    </div>
+  )
+}
+
+export default NotePage
